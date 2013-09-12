a7f9cd31876b43d57829fcef98e47999e5ca00ad
Ilya Kulakov <kulakov.ilya@gmail.com>
2013-09-13 04:22:08 +0700

    Update ShortcutRecorder to most recent version.

--- ShortcutRecorderDemo/IKDemoWindowController.m
+++ ShortcutRecorderDemo/IKDemoWindowController.m
@@ -46,6 +46,44 @@
     [[PTHotKeyCenter sharedCenter] resume];
 }
 
+- (BOOL)shortcutRecorder:(SRRecorderControl *)aRecorder shouldUnconditionallyAllowModifierFlags:(NSUInteger)aModifierFlags forKeyCode:(unsigned short)aKeyCode
+{
+    // Keep required flags required.
+    if ((aModifierFlags & aRecorder.requiredModifierFlags) != aRecorder.requiredModifierFlags)
+        return NO;
+
+    // Don't allow disallowed flags.
+    if ((aModifierFlags & aRecorder.allowedModifierFlags) != aModifierFlags)
+        return NO;
+
+    switch (aKeyCode)
+    {
+        case kVK_F1:
+        case kVK_F2:
+        case kVK_F3:
+        case kVK_F4:
+        case kVK_F5:
+        case kVK_F6:
+        case kVK_F7:
+        case kVK_F8:
+        case kVK_F9:
+        case kVK_F10:
+        case kVK_F11:
+        case kVK_F12:
+        case kVK_F13:
+        case kVK_F14:
+        case kVK_F15:
+        case kVK_F16:
+        case kVK_F17:
+        case kVK_F18:
+        case kVK_F19:
+        case kVK_F20:
+            return YES;
+        default:
+            return NO;
+    }
+}
+
 
 #pragma mark SRValidatorDelegate
 
